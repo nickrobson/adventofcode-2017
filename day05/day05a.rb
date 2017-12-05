20411a35b33572bd056e217b96fbacd061848fc4
Nick Robson <robson.nicholas1@gmail.com>
2017-12-05 20:43:20 +1100

    Day 5

---
+++ day05/day05a.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def execute(instructions, pc)
+	current = instructions[pc]
+	instructions[pc] += 1
+	pc + current
+end
+
+instructions = IO.readlines('input.txt').map! { |a| Integer(a) }
+pc, steps = 0, 0
+
+while pc >= 0 && pc < instructions.length 
+	pc = execute instructions, pc
+	steps += 1
+end
+
+p steps
